f900eebd9344a0bf7aff68972bd17c60b5e92b34
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-02-05 10:21:05 +0800

    添加命令行

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,13 +1,20 @@
 <?php
 declare(strict_types=1);
 
+namespace kooditorm\hyperctl;
+
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
             'dependencies' => [],
-            'commands' => [],
+            'commands' => [
+                StartServerCommand::class,
+                StopServerCommand::class,
+                RestartServerCommand::class,
+                StatusServerCommand::class
+            ],
             'listeners' => [],
             'annotations' => [
                 'scan' => [
